bb487d21034fb7b482ef9e816f0bee11394c857a
Estiven Pinillo <estivenpinillo.software.hotmail.com>
2024-05-22 23:51:08 -0500

    feat: add function msjMenu on MsjPrint view and menu on ToDoController

--- Controller/ToDoController.php
+++ Controller/ToDoController.php
@@ -13,12 +13,42 @@ class ToDoController {
         $mjsPrint = new MsjPrint();
         $usuario = new Usuario();
 
-        if($nombre != null){
+        if($nombre){
             $mjsPrint->clearScreen();
             $usuario->setNombre($nombre);
             $mjsPrint->msjBienvenidaUsuario($usuario);
+            $this->menu();
         }else {
             $mjsPrint->msjFin();
+        }
+
+    }
+
+    public function menu(){
+        $mjsPrint = new MsjPrint();
+        $opcion = $mjsPrint->msjMenu();
+        
+        switch ($opcion) {
+
+            case 1:
+                print("Opcion 1");
+                break;
+
+            case 2:
+                print("Opcion 2");
+                break;
+            
+            case 3:
+                print("Opcion 3");
+                break;
+
+            case 4:
+                print("Opcion 4");
+                break;
+
+            default:
+                print("No exite esa opcion.");
+                break;
         }
 
     }
